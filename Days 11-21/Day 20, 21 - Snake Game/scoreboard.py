2a26fb91dabd0c331656c3ef0397cbaafcb4d29f
antoniastanila <antoniamaria.stanila@gmail.com>
2025-08-19 18:28:11 +0300

    Added High Score to Snake Game, day 20

--- Days 11-21/Day 20, 21 - Snake Game/scoreboard.py
+++ Days 11-21/Day 20, 21 - Snake Game/scoreboard.py
@@ -7,26 +7,39 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.read_hs()
         self.speed("fastest")
         self.pencolor("white")
         self.penup()
         self.score_position()
-        self.write_text()
+        self.update_score()
         self.hideturtle()
 
     def score_position(self):
         self.goto(0, 250)
 
-    def write_text(self):
-        self.write(f"Score: {self.score}", False,
+    def update_score(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", False,
                    align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
-        self.write_text()
+        self.update_score()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", False,
-                   align=ALIGNMENT, font=FONT)
+    def read_hs(self):
+        with open("data.txt", "r") as file:
+            self.high_score = int(file.read())
+
+    def reset(self):
+        if self.score > self.high_score:
+            with open("data.txt", "w") as data:
+                data.write(str(self.score))
+        self.read_hs()
+        self.score = 0
+        self.update_score()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", False,
+    #                align=ALIGNMENT, font=FONT)
