5696cc066004a7b1a08fc4298d1f8df38443b3c7
antoniastanila <antoniamaria.stanila@gmail.com>
2026-06-19 18:08:51 +0300

    Completed Day 53

--- Days 50-59/Day 53 - Data Entry Job Automation/main_bs.py
+++ Days 50-59/Day 53 - Data Entry Job Automation/main_bs.py
@@ -7,7 +7,12 @@ load_dotenv()
 
 ZILLOW_URL = os.getenv("ZILLOW_URL")
 
-response = requests.get(url=ZILLOW_URL)
+header = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36",
+    "Accept-Language": "en-US,fr-FR;q=0.9,fr;q=0.8,ro-RO;q=0.7,ro;q=0.6,en;q=0.5"
+}
+
+response = requests.get(url=ZILLOW_URL, headers=header)
 response.raise_for_status()
 
 soup = BeautifulSoup(response.text, "html.parser")
--- Days 50-59/Day 53 - Data Entry Job Automation/main_selenium.py
+++ Days 50-59/Day 53 - Data Entry Job Automation/main_selenium.py
@@ -3,7 +3,9 @@ import os
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+import time
 load_dotenv()
 
 FORM_URL = os.getenv("GOOGLE_FORM_URL")
@@ -13,8 +15,24 @@ chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
 driver.get(FORM_URL)
+wait = WebDriverWait(driver, 10)
 
-input_elements = driver.find_elements(
-    By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+for index in range(len(links)):
+    address_input = wait.until(ec.presence_of_element_located(
+        (By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')))
+    address_input.send_keys(addresses[index])
 
-input_elements[0].send_keys("aaa")
+    price_input = wait.until(ec.presence_of_element_located(
+        (By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')))
+    price_input.send_keys((prices[index]))
+
+    link_input = wait.until(ec.presence_of_element_located(
+        (By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')))
+    link_input.send_keys(links[index])
+
+    sumbit_button = driver.find_element(
+        By.XPATH, value='/html/body/div/div[2]/form/div[2]/div/div[3]/div[1]/div[1]/div/span/span')
+    sumbit_button.click()
+    next_response = wait.until(ec.presence_of_element_located(
+        (By.XPATH, '/html/body/div[1]/div[2]/div[1]/div/div[4]/a')))
+    next_response.click()
