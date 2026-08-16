f79e318e4cbebc6da1480e8c10820b181a725720
Anto <antoniamaria.stanila@gmail.com>
2026-08-16 15:30:46 +0300

    Flask, WTForms and Bootstrap + CSV
    Finished Day 62: Struggled with writing in the csv, but the rest was quite fun.
    Should check out Angela's code tomorrow too and come with potential modifications.

--- Days 60-69/Day 62 - Flask, WTForms, Bootstrap and CSV/m.py
+++ Days 60-69/Day 62 - Flask, WTForms, Bootstrap and CSV/m.py
@@ -1,9 +1,23 @@
-import csv
+dict = {
+'csrf_token': 'Ijc1Y2M3NjkyOGU4MTgzYjMyYWM5YmE0MGY4NWM4YmRmYTgwYWQ3MmYi.aoGehA.ehA6L6RWmvyjk_t0LKsPZaqoxk8', 
 
-with open('cafe-data.csv', newline='', encoding='utf-8') as csv_file:
-        csv_data = csv.reader(csv_file, delimiter=',')
-        list_of_rows = []
-        for row in csv_data:
-            list_of_rows.append(row)
+'cafe': "Anto's Cafe", 
 
-print(list_of_rows)
+'cafe_location': 'https://www.google.com/maps/place/Lighthaus/@51.5701279,-0.0392317,19z/data=!3m1!4b1!4m6!3m5!1s0x48761db0ffba586b:0x93a04ceef20365f1!8m2!3d51.5701279!4d-0.0392317!16s%2Fg%2F11c6ttrf0p?entry=ttu&g_ep=EgoyMDI2MDgxMi4wIKXMDSoASAFQAw%3D%3D', 
+
+'opening_time': '8AM', 
+
+'closing_time': '9PM', 
+
+'coffee_rating': '3', 
+
+'wifi_rating': '4', 
+
+'socket_availability': '2', 
+
+'submit': 'Submit'
+}
+dict.pop('csrf_token')
+dict.pop('submit')
+data_list = [dict[elem] for elem in dict]
+print(data_list)
--- Days 60-69/Day 62 - Flask, WTForms, Bootstrap and CSV/main.py
+++ Days 60-69/Day 62 - Flask, WTForms, Bootstrap and CSV/main.py
@@ -1,22 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import DataRequired, URL
 import csv
+from werkzeug.datastructures import ImmutableMultiDict
 
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -25,6 +14,12 @@ Bootstrap5(app)
 
 class CafeForm(FlaskForm):
     cafe = StringField('Cafe name', validators=[DataRequired()])
+    cafe_location = StringField('Cafe Location on Google Maps (URL)', validators=[DataRequired(), URL()])
+    opening_time = StringField('Opening Time e.g. 8AM', validators=[DataRequired()])
+    closing_time = StringField('Closing Time e.g. 5:30AM', validators=[DataRequired()])
+    coffee_rating = SelectField('Coffee Rating', choices=[(1,"☕️"),(2,"☕️☕️"), (3,"☕️☕️☕️"), (4,"☕️☕️☕️☕️"), (5,"☕️☕️☕️☕️☕️")], validators=[DataRequired()])
+    wifi_rating = SelectField('WiFi Strength Rating', choices=[(1,"💪"),(2,"💪💪"), (3,"💪💪💪"), (4,"💪💪💪💪"), (5,"💪💪💪💪💪")], validators=[DataRequired()])
+    socket_availability = SelectField('Power Socket Availability', choices=[(1, '🔌'), (2, '🔌🔌'), (3, '🔌🔌🔌'), (4, '🔌🔌🔌🔌'), (5, '🔌🔌🔌🔌🔌')], validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 # Exercise:
@@ -42,11 +37,25 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/add')
+@app.route('/add', methods = ['POST', 'GET'])
 def add_cafe():
     form = CafeForm()
     if form.validate_on_submit():
         print("True")
+
+        cafe_dictionary = request.form.to_dict()
+        print(cafe_dictionary)
+ 
+        cafe_dictionary.pop('submit')
+        cafe_dictionary.pop('csrf_token')
+ 
+        data_list = [cafe_dictionary[elem] for elem in cafe_dictionary]
+        print(data_list)
+
+        with open("cafe-data.csv", "a", encoding="utf-8") as f:
+            f.write(",".join(data_list) + "\n")
+       
+        return redirect(url_for("cafes"))    
     # Exercise:
     # Make the form write a new row into cafe-data.csv
     # with   if form.validate_on_submit()
