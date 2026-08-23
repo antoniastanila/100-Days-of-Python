75ab5d51e9ec8ea5c36da19ca029aabdc16aaa36
antoniastanila <142498734+antoniastanila@users.noreply.github.com>
2026-08-23 13:44:16 +0300

    Added SQLAlchemy to books project
    finished day 63

--- Days 60-69/Day 63 - Creating a Virtual Bookshelf/main.py
+++ Days 60-69/Day 63 - Creating a Virtual Bookshelf/main.py
@@ -1,24 +1,82 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
 
 app = Flask(__name__)
 
-all_books = []
+class Base(DeclarativeBase):
+    pass
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
+
+db = SQLAlchemy(model_class=Base)
+
+db.init_app(app)
+
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+
+    def __repr__(self):
+        return f'<Book {self.title}>'
 
 
+with app.app_context():
+    db.create_all()
 
-@app.route('/')
+
+@app.route('/', methods = ['POST', 'GET'])
 def home():
-    return render_template("index.html", all_books = all_books, books_number = len(all_books))
+    # Construct a query to select from the database. Returns the rows in the database
+    result = db.session.execute(db.select(Book).order_by(Book.title))
+
+    # Used .scalars() to get the elements rather than entire rows from the database
+    all_books = result.scalars().all()
+    return render_template("index.html", all_books = all_books)
 
 
 @app.route("/add", methods = ['POST', 'GET'])
 def add():
+    book = Book()
     if request.method == 'POST':
-        book = request.form.to_dict()
-        all_books.append(book)
-        print(all_books)
+        form_entry = request.form.to_dict()
+
+        book.title = form_entry["bookname"]
+        book.author = form_entry["bookauthor"]
+        book.rating = form_entry["rating"]
+
+        db.session.add(book)
+        db.session.commit()
+
         return redirect(url_for('home'))
     return render_template("add.html")
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    if request.method == 'POST':
+        book_id = request.form["id"]
+        book_to_update = db.get_or_404(Book, book_id)
+        book_to_update.rating = float(request.form["rating"])
+        db.session.commit()
+        return redirect(url_for("home"))
+    book_id = request.args.get("id")
+    book_selected = db.get_or_404(Book, book_id)
+    return render_template("edit.html", book = book_selected)
+
+@app.route("/delete")
+def delete():
+    book_id = request.args.get('id')
+
+    # DELETE A RECORD BY ID
+    book_to_delete = db.get_or_404(Book, book_id)
+    # Alternative way to select the book to delete.
+    # book_to_delete = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
 
 
 if __name__ == "__main__":
