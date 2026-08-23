0bcf0946ee2e3ade1045a1531ac17c70f0f92a84
antoniastanila <142498734+antoniastanila@users.noreply.github.com>
2026-08-23 11:53:40 +0300

    CRUD operations

--- Days 60-69/Day 63 - Creating a Virtual Bookshelf/practice/main.py
+++ Days 60-69/Day 63 - Creating a Virtual Bookshelf/practice/main.py
@@ -1,13 +1,3 @@
-# import sqlite3
-
-# db = sqlite3.connect("books-collection.db")
-# cursor = db.cursor()
-
-# # cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
-
-# cursor.execute("INSERT INTO books VALUES(2, 'Harry Potqter', 'J. K. Rowling', '9.3')")
-# db.commit()
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
@@ -44,7 +34,45 @@ with app.app_context():
     db.create_all()
 
 # CREATE RECORD
+# with app.app_context():
+#     new_book = Book(id=1, title="Harry Potter", author="J. K. Rowling", rating=9.3)
+#     db.session.add(new_book)
+#     db.session.commit()
+
+# NOTE: When creating new records, the primary key fields is optional. you can also write:
+# new_book = Book(title="Harry Potter", author="J. K. Rowling", rating=9.3)
+# the id field will be auto-generated.
+
+
+
+# READ ALL RECORDS
 with app.app_context():
-    new_book = Book(id=1, title="Harry Potter", author="J. K. Rowling", rating=9.3)
-    db.session.add(new_book)
+    result = db.session.execute(db.select(Book).order_by(Book.title))
+    all_books = result.scalars()
+
+# READ A PARTICULAR RECORD By Query
+with app.app_context():
+    book = db.session.execute(db.select(Book).where(Book.title == "Harry Potter")).scalar()
+    print(book)
+
+# UPDATE A PARTICULAR RECORD By Query
+with app.app_context():
+    book_to_update = db.session.execute(db.select(Book).where(Book.title == "Harry Potter")).scalar()
+    book_to_update.title = "Harry Potter and the Goblet of Fire"
+    db.session.commit() 
+
+# UPDATE A PARTICULAR RECORD BY PRIMARY KEY
+book_id = 1
+with app.app_context():
+    book_to_update = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+    # or book_to_update = db.get_or_404(Book, book_id)  
+    book_to_update.title = "Harry Potter and the Goblet of Fire"
+    db.session.commit()  
+
+# DELETE A PARTICULAR RECORD BY PRIMARY KEY
+book_id = 1
+with app.app_context():
+    book_to_delete = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+    # or book_to_delete = db.get_or_404(Book, book_id)
+    db.session.delete(book_to_delete)
     db.session.commit()
