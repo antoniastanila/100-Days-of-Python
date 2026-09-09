83b6d60d514b5667c2e0b70172d64162a9716992
antoniastanila <142498734+antoniastanila@users.noreply.github.com>
2026-09-09 15:21:19 +0300

    Serialization
    This was supposes to work, but flask looks for the data elsewhere. Solution coming up in the next commit!

--- Days 60-69/Day 66  - Building an API with RESTful Routing/main.py
+++ Days 60-69/Day 66  - Building an API with RESTful Routing/main.py
@@ -2,20 +2,7 @@ from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Boolean
-
-'''
-Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
-
+from random import choice
 app = Flask(__name__)
 
 # CREATE DB
@@ -50,6 +37,27 @@ with app.app_context():
 def home():
     return render_template("index.html")
 
+@app.route("/random")
+def get_random_cafe():
+    result = db.session.execute(db.select(Cafe))
+    all_cafes = result.scalars().all()
+    print(all_cafes)
+    print(len(all_cafes))
+    random_cafe = choice(all_cafes)
+
+    return jsonify(cafe={
+        "id": random_cafe.id,
+        "name": random_cafe.name,
+        "map_url": random_cafe.map_url,
+        "img_url": random_cafe.img_url,
+        "location": random_cafe.location,
+        "seats": random_cafe.seats,
+        "has_toilet": random_cafe.has_toilet,
+        "has_wifi": random_cafe.has_wifi,
+        "has_sockets": random_cafe.has_sockets,
+        "can_take_calls": random_cafe.can_take_calls,
+        "coffee_price": random_cafe.coffee_price,
+    })
 
 # HTTP GET - Read Record
 
