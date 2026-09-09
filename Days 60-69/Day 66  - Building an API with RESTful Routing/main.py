a332805d85f15465b5945ea4b6a040518669e4fb
antoniastanila <142498734+antoniastanila@users.noreply.github.com>
2026-09-09 17:59:12 +0300

    Returned all cafes, pay attention
    Ok so jsonify works for dctionaries or lists of dictionaries. In this example, cafes is a kwarg, but you could just as easily skipped using it.
    
    Here's how Angela did it using list comprehension:
    return jsonify(cafes=[cafe.to_dict() for cafe in all_cafes])

--- Days 60-69/Day 66  - Building an API with RESTful Routing/main.py
+++ Days 60-69/Day 66  - Building an API with RESTful Routing/main.py
@@ -59,20 +59,22 @@ def get_random_cafe():
     print(len(all_cafes))
     random_cafe = choice(all_cafes)
 
-    return jsonify(cafe={
-        "id": random_cafe.id,
-        "name": random_cafe.name,
-        "map_url": random_cafe.map_url,
-        "img_url": random_cafe.img_url,
-        "location": random_cafe.location,
-        "seats": random_cafe.seats,
-        "has_toilet": random_cafe.has_toilet,
-        "has_wifi": random_cafe.has_wifi,
-        "has_sockets": random_cafe.has_sockets,
-        "can_take_calls": random_cafe.can_take_calls,
-        "coffee_price": random_cafe.coffee_price,
-    })
+    random_cafe = random_cafe.to_dict()
 
+    return jsonify(cafe=random_cafe)
+
+@app.route("/all")
+def get_all_cafes():
+    result = db.session.execute(db.select(Cafe))
+    all_cafes = result.scalars().all()
+
+    all_cafes = [cafe.to_dict() for cafe in all_cafes]
+
+    print(all_cafes[0])
+
+    return jsonify(
+        cafes = all_cafes
+    )
 # HTTP GET - Read Record
 
 # HTTP POST - Create Record
