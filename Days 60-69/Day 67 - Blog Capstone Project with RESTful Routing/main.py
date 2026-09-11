7c045925fd715718fe5ef3db1605e481e70f150f
antoniastanila <142498734+antoniastanila@users.noreply.github.com>
2026-09-11 14:46:53 +0300

    nimic wow

--- Days 60-69/Day 67 - Blog Capstone Project with RESTful Routing/main.py
+++ Days 60-69/Day 67 - Blog Capstone Project with RESTful Routing/main.py
@@ -45,20 +45,22 @@ with app.app_context():
 
 @app.route('/')
 def get_all_posts():
-    # TODO: Query the database for all the posts. Convert the data to a python list.
     posts = db.session.execute(db.select(BlogPost)).scalars().all()
 
     return render_template("index.html", all_posts=posts)
 
-# TODO: Add a route so that you can click on individual posts.
-@app.route('/')
+@app.route('/<post_id>')
 def show_post(post_id):
     # TODO: Retrieve a BlogPost from the database based on the post_id
-    requested_post = "Grab the post from your database"
+    requested_post = db.get_or_404(BlogPost, post_id)
     return render_template("post.html", post=requested_post)
 
 
 # TODO: add_new_post() to create a new blog post
+@app.route("/new_post", methods = ['GET', 'POST'])
+def add_new_post():
+    return render_template("make-post.html")
+
 
 # TODO: edit_post() to change an existing blog post
 
