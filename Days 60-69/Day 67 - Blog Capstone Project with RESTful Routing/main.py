e39d3ec63fa49575ce8faf8d64579c312c5a0213
antoniastanila <142498734+antoniastanila@users.noreply.github.com>
2026-09-13 18:13:17 +0300

    Delete Post

--- Days 60-69/Day 67 - Blog Capstone Project with RESTful Routing/main.py
+++ Days 60-69/Day 67 - Blog Capstone Project with RESTful Routing/main.py
@@ -115,6 +115,13 @@ def edit_post(post_id):
 
 # TODO: delete_post() to remove a blog post from the database
 
+@app.route("/delete/<post_id>")
+def delete_post(post_id):
+    post_to_delete = db.get_or_404(BlogPost, post_id)
+    db.session.delete(post_to_delete)
+    db.session.commit()
+    return redirect(url_for('get_all_posts'))
+
 @app.route("/about")
 def about():
     return render_template("about.html")
